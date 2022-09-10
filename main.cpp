f13dca613fe5e9872a762a138fd371c66c4c61af
lowe <lowe.raivio@gmail.com>
2022-09-10 20:49:23 +0200

    Cleanup (it works now...)

--- main.cpp
+++ main.cpp
@@ -2,18 +2,14 @@
 
 
 
-
 #include "tracy/Tracy.hpp"
-//#include "eng/src/Engine.h"
 #include "Engine.h"
 #include "TestScene.h"
-//#include "src/TestScene.h"
-
 
 int main(int argc, char* argv[])
 {
     {
-        Engine engine;
+        Engine engine;                
         engine.run(new TestScene(engine.sceneHandler)); 
     }
 
