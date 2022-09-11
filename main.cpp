9d63efe812fe368d41118369912a6480d97b6e3b
lowe <lowe.raivio@gmail.com>
2022-09-11 16:26:24 +0200

    FIX: Updating example file and cmake fetch source

--- main.cpp
+++ main.cpp
@@ -1,17 +1,32 @@
+#define _CRTDBG_MAP_ALLOC
+#include <stdio.h>
+#include <stdlib.h>
+#if defined(WIN32) && defined(_DEBUG)
+#include <crtdbg.h>
+#endif // WIN32 && _DEBUG
+
 #include <cstdlib>
 
-
-
+//#include <assimp/aabb.h>
 #include "tracy/Tracy.hpp"
-#include "Engine.h"
-#include "TestScene.h"
+#include "Engine.hpp"
+#include "TestScene.hpp"
+
 
 int main(int argc, char* argv[])
 {
+    // Set flags for tracking CPU memory leaks
+#if defined(WIN32) && defined(_DEBUG)
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif // WIN32 && _DEBUG
+
     {
-        Engine engine;                
-        engine.run(new TestScene(engine.sceneHandler)); 
+        Engine engine;
+        engine.run(new TestScene()); 
     }
+#if defined(WIN32) && defined(_DEBUG)
+    _CrtDumpMemoryLeaks();
+#endif // WIN32 && _DEBUG
 
     return EXIT_SUCCESS;
 }
