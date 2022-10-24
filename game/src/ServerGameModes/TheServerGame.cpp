8ec26d28f2d22f00f6851483bbfcf30ceff1f829
SimonStrands <etta11@hotmail.se>
2022-10-24 13:08:22 +0200

    updated example code

--- game/src/ServerGameModes/TheServerGame.cpp
+++ game/src/ServerGameModes/TheServerGame.cpp
@@ -7,29 +7,25 @@ TheServerGame::TheServerGame()
   roomHandler.generateRoom();
 }
 #include <iostream>
+void TheServerGame::init() {
+
+}
+
 void TheServerGame::update(float dt) 
 {
-    static bool yes = false;
-    if (players[0].position.z > 1 && !yes) {
-        yes = true;
-        std::cout << "spawn enemies" << std::endl;
-        serverEntities.push_back(
-            ServerEntity { glm::vec3(-0.83, 0, 126), glm::vec3(0, 0, 0), 1 });
-        addEvent({ (int)GameEvents::SpawnEnemy, 1 }, { -0.83f, 0.f, 126.f });
-        serverEntities.push_back(
-            ServerEntity { glm::vec3(-0.83, 0, 126), glm::vec3(0, 0, 0), 1 });
-        addEvent({ (int)GameEvents::SpawnEnemy, 1 }, { -0.83f, 0.f, 126.f });
+  static bool wentIn = false;
+  if (this->scene->getComponent<Transform>(this->scene->getPlayer(0))
+              .position.z > 0 &&
+      !wentIn)
+    {
+      std::cout << "step over 1" << std::endl;
+      wentIn = true;
+      this->scene->createEnemy(1);
     }
-
-    for (int i = 0; i < serverEntities.size(); i++) {
-        serverEntities[i].rotation.x += dt * 5.f * ((i * 0.25f) + 1.f);
-        if (serverEntities[i].rotation.x > 360) {
-            serverEntities[i].rotation.x = 0;
-        }
-        glm::vec3 dir =
-            pf.getDirTo(serverEntities[i].position, players[0].position) *
-            (dt * 20);
-        serverEntities[i].position += dir;
+  for (int i = 0; i < this->scene->getEnemySize(); i++)
+    {
+      this->scene->getComponent<Transform>(this->scene->getEnemies(i))
+          .rotation += dt * 50 * (i + 1);
     }
     
 }
--- game/src/ServerGameModes/TheServerGame.h
+++ game/src/ServerGameModes/TheServerGame.h
@@ -1,15 +1,16 @@
 #pragma once
-#include "../vengine/vengine/network/ServerGame.h"
+#include "../vengine/vengine/network/ServerEngine/ServerGame.h"
 #include "../vengine/vengine/ai/PathFinding.h"
 #include "../World Handling/RoomHandler.h"
 
-class TheServerGame : public ServerGame
+class TheServerGame : public ServerGameMode
 {
  private:
   RoomHandler roomHandler;
 
  public:
   TheServerGame();
+  void init();
   void update(float dt);
 
 };
