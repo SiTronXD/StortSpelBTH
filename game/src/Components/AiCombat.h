ab7ab5632cc202a19825a3c36cf0258ae4592546
Simadra <81558113+Simadra@users.noreply.github.com>
2022-10-24 10:49:49 +0200

    CombatAi Fixing

--- game/src/Components/AiCombat.h
+++ game/src/Components/AiCombat.h
@@ -1,13 +1,11 @@
 #pragma once
 #include <iostream>
 #include <vector>
-#include <chrono>
 
 struct AiCombat
 {
 	float lightHit = 0.f;
 	float lightAttackTime = 0.f;
-	bool attackActive = false;
-	std::chrono::time_point<std::chrono::system_clock> timer;
-	std::chrono::duration<float> hitTimer;
+	float lightAttackTime = 5.f;
+	float timer = 0.f;
 };
--- game/src/Systems/AiCombatSystem.hpp
+++ game/src/Systems/AiCombatSystem.hpp
@@ -1,40 +1,51 @@
-#pragma once
-
-#include <vengine.h>
-#include "../Components/AiCombat.h"
-
-class AiCombatSystem : public System
-{
-private:
-	Scene* scene;
-public:
-	AiCombatSystem(Scene* scene) : scene(scene)
-	{
-	}
-
-	bool update(entt::registry& reg, float dt) final
-	{
-		auto view = reg.view<AiCombat>();
-		auto foo = [&](AiCombat& combat)
-		{
-		};
-		view.each(foo);
-
-		return false;
-	}
-
-	void attack(AiCombat& combat)
-	{
-		if (!combat.attackActive)
-		{
-			combat.hitTimer = std::chrono::duration<float>(combat.lightAttackTime);
-			combat.timer = std::chrono::system_clock::now();
-
-			// Attack
-		}
-		else if ()
-		{
-
-		}
-	}
-};
+//#pragma once
+//
+//#include <vengine.h>
+//#include "../Components/AiCombat.h"
+//#include "../Components/AiMovement.h"
+//
+//class AiCombatSystem : public System
+//{
+//private:
+//	Scene* scene;
+//	SceneHandler* sceneHandler;
+//	int playerID;
+//public:
+//	AiCombatSystem(SceneHandler* sceneHandler) : sceneHandler(sceneHandler), playerID(-1)
+//	{
+//		scene = sceneHandler->getScene();
+//	}
+//
+//	bool update(entt::registry& reg, float dt) final
+//	{
+//		auto view = reg.view<AiCombat, AiMovement>();
+//		auto foo = [&](AiCombat& combat, AiMovement& movement)
+//		{
+//			if (combat.timer > 0.f)
+//			{
+//				combat.timer -= dt;
+//			}
+//			else if (movement.distance <= 10.f)
+//			{
+//				attack(combat, movement);
+//			}
+//		};
+//		view.each(foo);
+//
+//		return false;
+//	}
+//
+//	void attack(AiCombat& combat, AiMovement& movement)
+//	{
+//		if (playerID == -1)
+//		{
+//			Entity mainCamID = scene->getMainCameraID();
+//			sceneHandler->getScriptHandler()->getScriptComponentValue(
+//				scene->getComponent<Script>(mainCamID), playerID, "playerID");
+//		}
+//
+//		Combat& playerCombat = scene->getComponent<Combat>(playerID);
+//		playerCombat.health -= 10.f;
+//		std::cout << playerCombat.health << std::endl;
+//	}
+//};
