63438c1fc7d7f3872569a6e3b9a3ebe7446922e6
Oscar Nordström <oscar.nordstrom2000@gmail.com>
2022-10-26 15:19:50 +0200

    ADD: added definitions to transitions yeah

--- game/src/Ai/Behaviors/Swarm/SwarmFSM.cpp
+++ game/src/Ai/Behaviors/Swarm/SwarmFSM.cpp
@@ -5,30 +5,117 @@
 
 bool SwarmFSM::idle_combat(uint32_t entityID)
 {
-	return false;
+	bool ret = false;
+	int playerID;
+	std::string playerString = "playerID";
+    sceneHandler->getScriptHandler()->getGlobal(playerID, playerString);
+	Transform& playerTransform = FSM::sceneHandler->getScene()->getComponent<Transform>(playerID);
+	Transform& enemyTransform = FSM::sceneHandler->getScene()->getComponent<Transform>(entityID);
+	SwarmComponentBT& enemySwarmCompBT = FSM::sceneHandler->getScene()->getComponent<SwarmComponentBT>(entityID);
+	SwarmComponentFSM& enemySwarmCompFSM = FSM::sceneHandler->getScene()->getComponent<SwarmComponentFSM>(entityID);
+    
+	if (glm::length(enemyTransform.position - playerTransform.position) <=
+        enemySwarmCompBT.sightRadius && enemySwarmCompFSM.life > enemySwarmCompFSM.LOW_HEALTH)
+      {
+		ret = true;
+      }
+
+
+	return ret;
 }
 
 bool SwarmFSM::idle_escape(uint32_t entityID)
 {
-	return false;
+	bool ret = false;
+
+	int playerID;
+	std::string playerString = "playerID";
+    sceneHandler->getScriptHandler()->getGlobal(playerID, playerString);
+	Transform& playerTransform = FSM::sceneHandler->getScene()->getComponent<Transform>(playerID);
+	Transform& enemyTransform = FSM::sceneHandler->getScene()->getComponent<Transform>(entityID);
+	SwarmComponentBT& enemySwarmCompBT = FSM::sceneHandler->getScene()->getComponent<SwarmComponentBT>(entityID);
+	SwarmComponentFSM& enemySwarmCompFSM = FSM::sceneHandler->getScene()->getComponent<SwarmComponentFSM>(entityID);
+    
+	if (glm::length(enemyTransform.position - playerTransform.position) <=
+        enemySwarmCompBT.sightRadius && enemySwarmCompFSM.life <= enemySwarmCompFSM.LOW_HEALTH)
+      {
+		ret = true;
+      }
+
+	return ret;
 }
 
 bool SwarmFSM::combat_idle(uint32_t entityID)
 {
-	return false;
+	bool ret = false;
+
+	int playerID;
+	std::string playerString = "playerID";
+    sceneHandler->getScriptHandler()->getGlobal(playerID, playerString);
+	Transform& playerTransform = FSM::sceneHandler->getScene()->getComponent<Transform>(playerID);
+	Transform& enemyTransform = FSM::sceneHandler->getScene()->getComponent<Transform>(entityID);
+	SwarmComponentBT& enemySwarmCompBT = FSM::sceneHandler->getScene()->getComponent<SwarmComponentBT>(entityID);
+    
+	if (glm::length(enemyTransform.position - playerTransform.position) >
+        enemySwarmCompBT.sightRadius)
+      {
+		ret = true;
+      }
+
+
+	return ret;
 }
 
 bool SwarmFSM::combat_escape(uint32_t entityID)
 {
-	return false;
+	bool ret = false;
+
+	int playerID;
+	std::string playerString = "playerID";
+    sceneHandler->getScriptHandler()->getGlobal(playerID, playerString);
+	SwarmComponentFSM& enemySwarmCompFSM = FSM::sceneHandler->getScene()->getComponent<SwarmComponentFSM>(entityID);
+    
+	if (enemySwarmCompFSM.life <= enemySwarmCompFSM.LOW_HEALTH)
+    {
+		ret = true;
+    }
+
+	return ret;
 }
 
 bool SwarmFSM::escape_idle(uint32_t entityID)
 {
-	return false;
+	bool ret = false;
+
+	int playerID;
+	std::string playerString = "playerID";
+    sceneHandler->getScriptHandler()->getGlobal(playerID, playerString);
+	Transform& playerTransform = FSM::sceneHandler->getScene()->getComponent<Transform>(playerID);
+	Transform& enemyTransform = FSM::sceneHandler->getScene()->getComponent<Transform>(entityID);
+	SwarmComponentBT& enemySwarmCompBT = FSM::sceneHandler->getScene()->getComponent<SwarmComponentBT>(entityID);
+    
+	if (glm::length(enemyTransform.position - playerTransform.position) >
+        enemySwarmCompBT.sightRadius)
+    {
+		ret = true;
+    }
+
+	return ret;
 }
 
 bool SwarmFSM::escape_combat(uint32_t entityID)
 {
-	return false;
+	bool ret = false;
+
+	int playerID;
+	std::string playerString = "playerID";
+    sceneHandler->getScriptHandler()->getGlobal(playerID, playerString);
+	SwarmComponentBT& enemySwarmCompBT = FSM::sceneHandler->getScene()->getComponent<SwarmComponentBT>(entityID);
+    
+	if (enemySwarmCompBT.group->members.size() > 0)
+    {
+		ret = true;
+    }
+
+	return ret;
 }
