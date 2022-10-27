9f211479012b03be0c95336988b8f0f3a3ea3259
Simon <47039664+SiTronXD@users.noreply.github.com>
2022-10-27 11:59:31 +0200

    ADD: crosshair/perk slots UI

--- game/scripts/UI.lua
+++ game/scripts/UI.lua
@@ -13,7 +13,7 @@ function script:update(dt)
 	local xSize = 1024 * 0.35
 	local ySize = 64 * 0.35
 
-	local hpPercent = 0.8
+	local hpPercent = 0.3
 
 	-- Black HP background
     uiRenderer.setTexture(self.hpBarBackgroundTextureID)
@@ -22,6 +22,17 @@ function script:update(dt)
 	-- Red HP bar
     uiRenderer.setTexture(self.hpBarTextureID)
 	uiRenderer.renderTexture(xPos - (1.0 - hpPercent) * xSize * 0.5, yPos, xSize * hpPercent, ySize)
+
+	-- Perk slots
+	uiRenderer.setTexture(self.perkSlotTextureID)
+	for i = 1, 3 
+	do
+		uiRenderer.renderTexture(-xPos - 70 + i * 80, yPos, 70, 70)
+	end
+
+	-- Crosshair
+	uiRenderer.setTexture(self.crosshairTextureID)
+	uiRenderer.renderTexture(0, 0, 30, 30)
 end
 
 return script
--- game/scripts/gamescene.lua
+++ game/scripts/gamescene.lua
@@ -18,12 +18,19 @@ scene.getComponent(playerID, CompType.Script).camID = cam
 network.sendPlayer(player)
 
 -- UI
+local pixelArtSamplerSettings = {}
+pixelArtSamplerSettings.filterMode = Filters.Nearest
+
 local uiID = scene.createEntity()
 scene.setComponent(uiID, CompType.Script, "scripts/UI.lua")
 scene.getComponent(uiID, CompType.Script).hpBarBackgroundTextureID =
 	resources.addTexture("assets/textures/UI/hpBarBackground.png")
 scene.getComponent(uiID, CompType.Script).hpBarTextureID = 
 	resources.addTexture("assets/textures/UI/hpBar.png")
+scene.getComponent(uiID, CompType.Script).perkSlotTextureID = 
+	resources.addTexture("assets/textures/UI/perkSlot.png", pixelArtSamplerSettings)
+scene.getComponent(uiID, CompType.Script).crosshairTextureID = 
+	resources.addTexture("assets/textures/UI/crosshair.png", pixelArtSamplerSettings)
 
 --[[local p = scene.createPrefab("scripts/prefabs/prefab.lua")
 
