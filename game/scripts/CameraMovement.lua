3e2f8341522da2d44e9378ca55c4543cb3de63fb
Simadra <81558113+Simadra@users.noreply.github.com>
2022-11-10 13:42:35 +0100

    Small thing dont worry

--- game/scripts/CameraMovement.lua
+++ game/scripts/CameraMovement.lua
@@ -83,7 +83,7 @@ function script:update(dt)
     local actualDist = self.camDist + self.distMargin
     local payload = physics.raycast(targetPos, -forward, actualDist)
     if (payload) then
-        if(scene.getComponent(payload.entity, CompType.Collider).isTrigger == false and
+        if (scene.getComponent(payload.entity, CompType.Collider).isTrigger == false and
            payload.entity ~= self.playerID) then
             actualDist = vector.length(payload.hitPoint - targetPos)
         end
