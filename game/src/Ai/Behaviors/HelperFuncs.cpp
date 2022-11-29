76b3d4a6314055f08f64d15a6d70629a939adffb
lowe <lowe.raivio@gmail.com>
2022-11-29 09:41:59 +0100

    ADD: Helper getRandomDir func

--- game/src/Ai/Behaviors/HelperFuncs.cpp
+++ game/src/Ai/Behaviors/HelperFuncs.cpp
@@ -20,6 +20,14 @@ float lookAtY(const glm::vec3& from, const glm::vec3& to)
     return angle; 
 }
 
+const glm::vec3 getRandomDir(glm::vec3 manipulator)
+{
+    return glm::normalize(glm::vec3(
+        1/(float)(rand()%100)   * (rand()%2 == 0 ? 1.f : -1.f) * manipulator.x , 
+        1/(float)(rand()%100)   * (rand()%2 == 0 ? 1.f : -1.f) * manipulator.y , 
+        (1/(float)(rand()%100)) * (rand()%2 == 0 ? 1.f : -1.f) * manipulator.z ));
+}
+
 float getAngleBetween(const glm::vec3 one, const glm::vec3 two)
 {
     float angle_between			= glm::degrees(glm::acos(glm::dot(one, two)));
--- game/src/Ai/Behaviors/HelperFuncs.hpp
+++ game/src/Ai/Behaviors/HelperFuncs.hpp
@@ -6,3 +6,4 @@
 float lookAtY(const Transform& from, const Transform& to);
 float lookAtY(const glm::vec3& from, const glm::vec3& to);
 float getAngleBetween(const glm::vec3 one, const glm::vec3 two);
+const glm::vec3 getRandomDir(glm::vec3 manipulator = {1.f,1.f,1.f});
