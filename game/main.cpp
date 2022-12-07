5bfd7c824b0ca7a7c9186dfdcf729d71efc343fc
Simon <47039664+SiTronXD@users.noreply.github.com>
2022-12-07 16:28:12 +0100

    ADD: activate/deactivate perks/abilities

--- game/main.cpp
+++ game/main.cpp
@@ -42,8 +42,8 @@ int main(int argc, char* argv[])
     {
         Engine engine;
         engine.setCustomNetworkHandler(new NetworkHandlerGame());
-        engine.run("Presumed Dead", "", new LobbyScene());
-        //engine.run("Presumed Dead", "scripts/gamescene.lua", new GameScene());
+        //engine.run("Presumed Dead", "", new LobbyScene());
+        engine.run("Presumed Dead", "scripts/gamescene.lua", new GameScene());
         //engine.run("Presumed Dead", "", new RoomTesting());
         //engine.run("Presumed Dead", "scripts/MainMenu.lua", new MainMenu());
         //engine.run("Presumed Dead", "scripts/gamescene.lua", new GameScene());
