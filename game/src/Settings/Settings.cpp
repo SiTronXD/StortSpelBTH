ca31daaf35a33214549a6e44a58e483b079c0531
William Kamnert <wille.kamnert@gmail.com>
2022-12-07 16:07:57 +0100

    WIP: Settings struct

---
+++ game/src/Settings/Settings.cpp
@@ -0,0 +1,6 @@
+#include "Settings.h"
+
+float Settings::volume = 50.0f;
+bool Settings::fullScreen = false;
+float Settings::sensitivity = 2.0f;
+float Settings::cameraFOV = 90.0f;
---
+++ game/src/Settings/Settings.h
@@ -0,0 +1,7 @@
+struct Settings
+{
+	static float volume;
+	static bool fullScreen;
+	static float sensitivity;
+	static float cameraFOV;
+};
