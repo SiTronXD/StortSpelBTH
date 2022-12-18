9487458c0c0de2a64d2414d5c4be7d5f7bf96a77
lowe <lowe.raivio@gmail.com>
2022-12-18 12:58:38 +0100

    Change submodule

--- game/main.cpp
+++ game/main.cpp
@@ -27,7 +27,7 @@ int main(int argc, char* argv[])
 {
     // Set flags for tracking CPU memory leaks
 #if defined(_WIN32) && defined(_DEBUG)
-    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);    
 #endif // _WIN32 && _DEBUG
 
 #ifdef _CONSOLE 
